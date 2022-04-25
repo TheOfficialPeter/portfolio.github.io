000fe88a96f57d4fcb7d4c9c5356becb7669c6f1
Peter <57006688+TheOfficialPeter@users.noreply.github.com>
2022-04-25 14:09:49 +0100

    updates

---
+++ frontShowcase.js
@@ -0,0 +1,14 @@
+const titlesF = [
+    "Basic icon designs",
+    "Different frameworks"
+];
+
+const imgsF = [
+    "images/icons.png",
+    "images/frameworks.png"
+];
+
+const txtF = [
+    "I can make basic icons with a minimalistic design using Figma or MS Word (lol).",
+    "I have played around with different frameworks that can make a website look more modern and approachable."
+];
---
+++ robloxShowcase.js
@@ -0,0 +1,16 @@
+const titlesR = [
+    "Perlin Noise",
+    "Cellular Automata",
+    "Physics-based Systems",
+];
+
+const imgsR = [
+    "images/perlin.png",
+    "images/Cellular.png",
+];
+
+const txtR = [
+    "Algorithm used to create randomized noise.",
+    "Cellular automata used to make parts interactable with one another depending on their states and properties.",
+    "Physics-based systems used to add more realistic feels to games.",
+];
